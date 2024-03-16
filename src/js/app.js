e1e3c0ed4bc9ec137483f3df56cf76d005956d0b
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-03-16 22:30:48 +0100

    Layout Song element

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,11 @@
 
 import GreenAudioPlayer from '../../vendor/greenplayer/js/main.js';
 
-const allPlayersElems = document.querySelectorAll('.song__player');
+const allPlayersElems = document.querySelectorAll('.greenplayer');
 console.log('allPlayersElems', allPlayersElems);
 
 const players = GreenAudioPlayer.init({
-  selector: '.song__player', // inits Green Audio Player on each audio container that has class "player"
+  selector: '.greenplayer', // inits Green Audio Player on each audio container that has class "player"
   stopOthersOnPlay: true,
 });
 console.log(players);
